64af307bbd1a60ef69c272df8bf74a70c5036ebb
tinyfish <tifish@gmail.com>
2021-04-14 16:03:34 +0800

    Optimize option page messages.

--- OptionsPage.cs
+++ OptionsPage.cs
@@ -14,64 +14,79 @@ namespace Tinyfish.FormatOnSave
             Windows = 1,
         };
 
-        [Category("On Save")]
+        [Category("Remove and sort")]
+        [DisplayName("Enable remove and sort")]
         [Description("Enable remove and sort on save, only apply to .cs file.")]
         public bool EnableRemoveAndSort { get; set; } = false;
 
-        [Category("On Save")]
+        [Category("Remove and sort")]
+        [DisplayName("Enable smart remove and sort")]
         [Description("Apply remove and sort to .cs without #if. Remove and sort must be enabled first.")]
         public bool EnableSmartRemoveAndSort { get; set; } = true;
 
-        [Category("On Save")]
+        [Category("Format")]
+        [DisplayName("Enable format document")]
         [Description("Enable format document on save.")]
         public bool EnableFormatDocument { get; set; } = true;
 
-        [Category("On Save")]
-        [Description("Allow extentions for FormatDocument only. Space separated list. For example: .cs .html .cshtml .vb")]
+        [Category("Format")]
+        [DisplayName("Allowed extensions for FormatDocument only")]
+        [Description("Allowed extensions for FormatDocument only. Space separated list. For example: .cs .html .cshtml .vb")]
         public string AllowFormatDocumentExtentions { get; set; } = string.Empty;
 
-        [Category("On Save")]
-        [Description("Deny extentions for FormatDocument only. Space separated list. For example: .cs .html .cshtml .vb")]
+        [Category("Format")]
+        [DisplayName("Denied extensions for FormatDocument only")]
+        [Description("Denied extensions for FormatDocument only. Space separated list. For example: .cs .html .cshtml .vb")]
         public string DenyFormatDocumentExtentions { get; set; } = string.Empty;
 
-        [Category("On Save")]
+        [Category("Line break")]
+        [DisplayName("Enable unify line break")]
         [Description("Enable unify line break on save.")]
         public bool EnableUnifyLineBreak { get; set; } = false;
 
-        [Category("On Save")]
-        [Description("Allow extentions for all except FormatDocument. Space separated list. For example: .cs .html .cshtml .vb")]
-        public string AllowExtentions { get; set; } = string.Empty;
-
-        [Category("On Save")]
-        [Description(
-            "Deny extentions for all except FormatDocument. Space separated list. For example: .cs .html .cshtml .vb")]
-        public string DenyExtentions { get; set; } = string.Empty;
-
-        [Category("On Save")]
+        [Category("Line break")]
+        [DisplayName("Line break style")]
         [Description("Line break style.")]
         public LineBreakStyle LineBreak { get; set; } = LineBreakStyle.Unix;
 
-        [Category("On Save")]
+        [Category("Line break")]
+        [DisplayName("Enable unify end of file")]
         [Description("Enable unify end of file to one empty line on save.")]
         public bool EnableUnifyEndOfFile { get; set; } = true;
 
-        [Category("On Save")]
+        [Category("Others")]
+        [DisplayName("Allowed extensions for all except FormatDocument")]
+        [Description("Allowed extensions for all except FormatDocument. Space separated list. For example: .cs .html .cshtml .vb")]
+        public string AllowExtentions { get; set; } = string.Empty;
+
+        [Category("Others")]
+        [DisplayName("Denied extensions for all except FormatDocument")]
+        [Description(
+            "Denied extensions for all except FormatDocument. Space separated list. For example: .cs .html .cshtml .vb")]
+        public string DenyExtentions { get; set; } = string.Empty;
+
+        [Category("Others")]
+        [DisplayName("Enable tab to space")]
         [Description("Enable tab to space on save. Depends on tabs options for the type of file.")]
         public bool EnableTabToSpace { get; set; } = false;
 
-        [Category("On Save")]
+        [Category("UTF8")]
+        [DisplayName("Enable force file encoding to UTF8 with BOM")]
         [Description("Enable force file encoding to UTF8 with BOM on save.")]
         public bool EnableForceUtf8WithBom { get; set; } = false;
 
-        [Category("On Save")]
-        [Description("Allow extentions for ForceUtf8WithBom only. Space separated list. For example: .cs .html .cshtml .vb")]
+        [Category("UTF8")]
+        [DisplayName("Allowed extensions for ForceUtf8WithBom only")]
+        [Description("Allowed extensions for ForceUtf8WithBom only. Space separated list. For example: .cs .html .cshtml .vb")]
         public string AllowForceUtf8WithBomExtentions { get; set; } = string.Empty;
 
-        [Category("On Save")]
-        [Description("Deny extentions for ForceUtf8WithBom only. Space separated list. For example: .cs .html .cshtml .vb")]
+        [Category("UTF8")]
+        [DisplayName("Denied extensions for ForceUtf8WithBom only")]
+        [Description("Denied extensions for ForceUtf8WithBom only. Space separated list. For example: .cs .html .cshtml .vb")]
         public string DenyForceUtf8WithBomExtentions { get; set; } = string.Empty;
 
-        [Category("On Save")]
+        [Category("Others")]
+        [DisplayName("")]
         [Description("Enable remove trailing spaces. It is mostly for Visual Sutdio 2012, which won't remove trailing spaces when formatting. In higher version than 2012, this will do nothing when FormatDocument is enabled.")]
         public bool EnableRemoveTrailingSpaces { get; set; } = true;
 
